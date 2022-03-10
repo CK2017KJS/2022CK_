a08302a18b55456a6b5283901949016983c9b5d9
CK2017KJS <201751006chungkang@gmail.com>
2022-03-10 10:39:59 +0900

    OOP 2주차

--- OOPPrpogramming/Class02/Code/Code01.cpp
+++ OOPPrpogramming/Class02/Code/Code01.cpp
@@ -1,47 +1,74 @@
 
+//한글 인코딩 깨짐 방지
+
+
 #include <iostream>
-#include <vecotr>
-/*
-한글 인코딩 깨짐 방지-
-
-
-
-
-*/
-
-
 using namespace std;
 
-int iWidth,iHeight;
-
-cout<<"Enter Buffer Width:";
-cin>>iWidth;
-
-cout<<"Enter Buffer Height:";
-cin>>iHeight;
-
-vector<char*> VectorChar;
-
-for(int i=0; i<iHeight;i++)
+class CArrayProgram 
 {
-    char* pCharArray = new char[iWidth];
-    for(int j=0;j<iWidth;j++)
-    {
-        pCharArray[j] = (char)'A'+i;
-    }
-    VectorChar.push_back(pCharrArray);
-}
+	int m_iX, m_iY;
+	char** m_Array;
+private:
+	void RenderLine(int Line) 
+	{
+		for (int i = 0; i < m_iX; i++)
+			cout << m_Array[Line - 1][i];
+	}
+public:
+	void Init() 
+	{
+		cout << "Enter Buffer Width : ";
+		cin >> m_iX;
+		cout << "Enter Buffer Height: ";
+		cin >> m_iY;
+
+		m_Array = new char*[m_iX];
+
+		for (int i = 0; i < m_iY; i++) 
+		{
+			m_Array[i] = new char[m_iX];
+			for (int j = 0; m_iX; j++)
+				cout << m_Array[i][j];
+		}
+
+	}
+
+	void Run() 
+	{
+		int Input;
+		while (true) 
+		{
+			cout << "\n Enter PrintLine :";
+			cin >> Input;
+
+			if (!Input) 
+			{
+
+			}
 
 
+			else 
+			{
+				if (Input < m_iX)
+				{
+					this->RenderLine(Input);
+				}
+
+				else 
+				{
+					cout << "Index Over Error" << endl;
+				}
 
 
-for(int i=0; i<iHeight;i++)
-{
-    for(int j=0;j<iWidth;j++)
-    {
-        delete pCharArray[j];
-    }
+			}
+		}
+	}
 
-}
-
-
+	~CArrayProgram()//2차원 배열 초기화
+	{
+		for (int i = 0; i < m_iY; i++)
+			delete m_Array[i];
+		delete m_Array;
+	}
+};
