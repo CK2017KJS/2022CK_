0570c24f440bfc147008c601f436f43dfb7081b0
CK2017KJS <chungkane201751006@gmail.com>
2022-03-13 06:50:05 +0900

    주석 추가

--- GameNetWorkProgrammingGroundWork/02Assignment/ClientBased.cs
+++ GameNetWorkProgrammingGroundWork/02Assignment/ClientBased.cs
@@ -3,6 +3,10 @@ using System.Text;
 using System.IO; 
 using System.Net.Sockets; 
 
+//A data value that both the 
+//server and the client have.
+//포트,IP,버퍼 사이즈같은 값들을
+//Pulbic로 저장해두었습니다.
 namespace MyNetWork
 {
     public class NetWorkBaseSocket
@@ -11,6 +15,8 @@ namespace MyNetWork
         public int m_iPort;
         public int m_iBufferSize;  
 
+        //This Data OutPut
+        //현재 데이터를 출력합니다
         public void RenderData()
         {
             Console.WriteLine("Connect Sever. IP Adress: {0}",m_strSeverIp);
